a7b48b2470a42b3f10962f10734cadf26a2d9a4d
Yannick Rehberger <dev@rehberger.family>
2020-01-28 22:16:15 +0100

    Implement a simple echo server.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # vrb2json
 Simple HTTP service, that converts VRB statements into json.
+
+## Run
+You can simply run the service locally in your Virtualenv.
+
+```
+FLASK_APP=./src/app.py flask run
+```
+
+Note that this is a development server. Do not use it in a production deployment.
+Use curl to validate availability.
+
+```
+curl http://localhost:5000/vrb2json/v1/info
+```
--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import (Flask, request)
+import csv
+import codecs
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/vrb2json/v1/convert', methods=['POST'])
+def convert():
+    return request.get_data()
+
+@app.route('/vrb2json/v1/info', methods=['GET'])
+def info():
+    return 'This is the vrs2json service.'
